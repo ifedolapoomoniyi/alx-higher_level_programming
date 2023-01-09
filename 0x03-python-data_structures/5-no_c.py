c33818c5e22bfd4d59d69cfa4359b352840b195c
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-01-09 01:46:10 +0000

    bug fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    copy = [char for char in my_string if char != 'c' and char != 'C']
+    return ("".join(copy))
