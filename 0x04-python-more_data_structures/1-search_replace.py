ed79569538d041b092540a5187085aaaf52bf92d
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-01-12 03:11:05 +0000

    bug fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for x in my_list:
-        if x == search:
-            new_list.append(replace)
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
         else:
-            new_list.append(x)f
-    return new_list
+            copy.append(my_list[i])
+    return copy
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-roman_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                'M': 1000, '0': 0}
-
-
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+    if not roman_string or type(roman_string) != str:
         return 0
-    roman_string += '0'
-    i, sum = 0, 0
-    while i < len(roman_string) - 1:
-        this = roman_digits[roman_string[i]]
-        next = roman_digits[roman_string[i + 1]]
-        if this == next:
-            sum += next + this
-            i += 1
-        elif this < next:
-            sum += next - this
-            i += 1
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
-            sum += this
-        i += 1
-    return sum`
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
