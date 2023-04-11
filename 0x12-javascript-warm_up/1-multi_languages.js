6ffbbba2b739be53857ed61c35bc7cef9def150a
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-04-11 18:28:58 +0100

    Javascript works

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//Print 3 sentences to the console
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const integer = parseInt(process.argv[2]);
+
+if (!integer) {
+  console.log('Not a number');
+} else console.log(`My number: ${integer}`)
+
