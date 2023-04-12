04011a8a5fc7eaff0cdad17b3d5e6ecb4b905f0a
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-04-11 19:43:57 -0700

    semistandard compliant + bug fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 const integer = parseInt(process.argv[2]);
 
-function factorial(num) {
-    if (isNaN(num || num === 1)) {
-        return 1;
-    }
-    return (num * factorial(num - 1));
+function factorial (num) {
+  if (isNaN(num) || num === 1) {
+    return 1;
+  }
+  return (num * factorial(num - 1));
 }
 
 console.log(factorial(integer));
