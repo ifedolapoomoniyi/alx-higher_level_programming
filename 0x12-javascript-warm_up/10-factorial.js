9b80a47d191155299d78e7ee549f8650f1abab94
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-04-12 03:36:53 +0100

    Updated function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,5 +2,11 @@
 
 const integer = parseInt(process.argv[2]);
 
-function factorial() {
+function factorial(num) {
+    if (isNaN(num || num === 1)) {
+        return 1;
+    }
+    return (num * factorial(num - 1));
+}
 
+console.log(factorial(integer));
