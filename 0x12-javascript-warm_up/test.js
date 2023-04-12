3f9e4f5f443e979b28f7e4b36eb984748a71129e
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-04-11 21:09:57 -0700

    semistandard compliant code

--- 0x12-javascript-warm_up/test.js
+++ 0x12-javascript-warm_up/test.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
+  type: 'object',
+  value: 12
+};
 console.log(myObject);
 
-require('./103-object_fct')
+require('./103-object_fct');
 
-console.log(myObject)
+console.log(myObject);
