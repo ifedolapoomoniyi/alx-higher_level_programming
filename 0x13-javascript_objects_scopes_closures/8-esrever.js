1f37813b09523d44950dc48bc47ddb91e1c0641e
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-04-13 03:12:14 +0100

    Last basic tasks

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,9 @@
 
 exports.esrever = function (list) {
   const arr = [];
-
+  
   for (let i = list.length - 1; i > -1; i--) {
     arr.push(list[i])
   }
+  return arr
 }
--- 0x13-javascript_objects_scopes_closures/tests.js
+++ 0x13-javascript_objects_scopes_closures/tests.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
-const nbOccurences = require('./7-occurrences').nbOccurences;
+const converter = require('./10-converter').converter;
 
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
+let myConverter = converter(10);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+console.log(myConverter(16))
+
+
+myConverter = converter(16);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+console.log(myConverter(16))
