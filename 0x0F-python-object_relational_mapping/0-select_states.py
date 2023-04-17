e1088723d163e54c9a1785bbe7fd8124e704edf6
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-04-16 17:41:23 -0700

    checker error fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-
-"""A script that lists all states fro the database hbtn_0e_0_usa"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cursor = db.cursor()
-
-    cursor.execute("SELCECT * FROM states ORDER BY states.d ASC;")
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
     for r in rows:
         print(r)
