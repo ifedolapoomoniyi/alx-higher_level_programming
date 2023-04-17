05f69d4dd3345f6c52a50e46835c34d03e766c06
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-04-16 17:35:13 -0700

    list all states in database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""A script that lists all states fro the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, 
+                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELCECT * FROM states ORDER BY states.d ASC;")
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
