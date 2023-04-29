6a5bf20e6d850863611509b8c9c2a2ea21577cdd
Dr Strange <ifedolapo0001@gmail.com>
2023-04-29 05:10:43 -0700

    function that returns the peak of an array

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a function that finds the peak of an unsorted array of numbers"""
+
+def find_peak(list_of_integers):
+    """"Sorts an array and returns the peak"""
+
+    if len(list_of_integers) == 0:
+        return None
+
+    array = list_of_integers
+
+    array.sort(reverse=True)
+
+    return array[0]
