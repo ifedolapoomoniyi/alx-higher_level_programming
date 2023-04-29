cdf0110af9551cc9f0aa2c1ca495e88e87dc1c95
Dr Strange <ifedolapo0001@gmail.com>
2023-04-29 15:10:04 -0700

    returns a formatted response from the given url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A python script that fetches the given url"""
+
+
+import urllib.request
+
+
+def main():
+    """ uses urllib to fetch data"""
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+    
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
