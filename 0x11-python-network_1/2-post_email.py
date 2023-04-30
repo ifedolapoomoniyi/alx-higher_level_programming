dbdc9ea160058bc6be0f2119ce776d13d9d7a96d
Dr Strange <ifedolapo0001@gmail.com>
2023-04-29 20:04:45 -0700

    Checker refused to pass my working code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-""" Sends a POST request to the passed URL with the email as parameter"""
-
-import urllib.request
-import urllib.parse
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
+"""
+from urllib import request, parse
 import sys
-
-url = sys.argv[1]
-email = sys.argv[2]
-
-
-def main():
-    """ takes in url and email from the system arguments
-        Sends a POST request with the url and email as arguments
-        returns the data encoded in utf-8
-    """
-
-
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
 
 
 if __name__ == "__main__":
-    main()
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
