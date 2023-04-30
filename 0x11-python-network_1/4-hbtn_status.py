bef3c9e2b2bfa6574338536f601908b38381dd90
Dr Strange <ifedolapo0001@gmail.com>
2023-04-29 20:44:44 -0700

    does the same as 0-hbtn_status.py but with the requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status').text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(r), r))
