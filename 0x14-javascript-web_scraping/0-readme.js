cb7a9306a32b26a9847149b55909f25093784dec
Dr Strange <ifedolapo0001@gmail.com>
2023-05-23 17:34:55 -0700

    semistandard compliant code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-//script that prints the content of a file
+// script that prints the content of a file
 
 const fs = require('fs');
-const file = process.argv[2]
+const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (error, content) => {
   if (error) {
-    console.log(eror);
+    console.log(error);
   } else {
     console.log(content);
-    }
+  }
 });
