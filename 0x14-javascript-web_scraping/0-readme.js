fe6d04ee795dad7603e4a659dd7c72c55ebaccd9
Dr Strange <ifedolapo0001@gmail.com>
2023-05-23 17:31:13 -0700

    node script that reads and prints the contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//script that prints the content of a file
+
+const fs = require('fs');
+const file = process.argv[2]
+
+fs.readFile(file, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(eror);
+  } else {
+    console.log(content);
+    }
+});
