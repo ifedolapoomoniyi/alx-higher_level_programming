5f85a985c8dfdbf5428542967e66107377f25a1b
Dr Strange <ifedolapo0001@gmail.com>
2023-05-23 18:08:43 -0700

    displays the result of an http GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Displays the status code of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log('code: ' + response.statusCode);
+});
